8907c5990a0b98f36d03e3260f4ec459c4741c96
Anusha <cdan.dharmasena@gmail.com>
2022-12-08 10:20:01 +1100

    make padding 8px wide

--- src/components/ProductMarketingCarousel/ProductMarketingCarouselDesktop.tsx
+++ src/components/ProductMarketingCarousel/ProductMarketingCarouselDesktop.tsx
@@ -1,24 +1,13 @@
 import React, { useRef } from "react";
 import Carousel from "react-multi-carousel";
 import styled from "styled-components";
-import globalConfig from "../../config";
 import config from "./config";
 import { CarouselItemData } from "./types";
 import LongCarouselItem from "../Long";
 
-const CarouselWrapper = styled.div`
-  grid-area: b;
-  position: relative;
-  padding-right: 70px;
-
-  @media (max-width: ${globalConfig.screenSizes.sm.max}px) {
-    padding-right: 0;
-  }
-`;
-
 const CarouselItemWrapper = styled.div`
-  padding-left: 7.5px;
-  padding-right: 7.5px;
+  padding-left: 8px;
+  padding-right: 8px;
 `;
 
 type Props = {
@@ -26,19 +15,17 @@ type Props = {
   items: Array<CarouselItemData>;
 };
 
-const ProductMarketingCarouselDesktop = ({ heading, items }: Props) => {
+const ProductMarketingCarouselDesktop = ({ items }: Props) => {
   const carouselRef = useRef<Carousel>(null);
 
   return (
-    <CarouselWrapper>
-      <Carousel {...config} ref={carouselRef} deviceType="desktop">
-        {items.map((feature, idx) => (
-          <CarouselItemWrapper key={idx}>
-            <LongCarouselItem {...feature} />
-          </CarouselItemWrapper>
-        ))}
-      </Carousel>
-    </CarouselWrapper>
+    <Carousel {...config} ref={carouselRef} deviceType="desktop">
+      {items.map((feature, idx) => (
+        <CarouselItemWrapper key={idx}>
+          <LongCarouselItem {...feature} />
+        </CarouselItemWrapper>
+      ))}
+    </Carousel>
   );
 };
 
